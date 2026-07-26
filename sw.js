778f8006d47aa5b131b187ff01cfdf4ede2c962c
Michele <mikdr96@gmail.com>
2026-07-26 22:33:24 +0200

    Update SW: network-first strategy for automatic updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tierlist-v1';
+const CACHE = 'tierlist-v2';
 const ASSETS = ['./', 'index.html', 'manifest.json'];
 
 self.addEventListener('install', e => {
@@ -15,6 +15,10 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./')))
+    fetch(e.request).then(r => {
+      const clone = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return r;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./')))
   );
 });
